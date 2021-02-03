3257bf3211825770721a45e201f5039f6c2ce053
dungtang93 <dungtang93@gmail.com>
2021-02-03 23:48:29 +0700

    make Ball fly at constant speed
    & some mumbo-jumbo here to spice up the gameplay!

--- src/game-objects/Ball/Ball.ts
+++ src/game-objects/Ball/Ball.ts
@@ -9,6 +9,11 @@ import { Interactable } from '../../app/Interactable'
 import { ballBody, defaultLayout } from '../../config'
 
 type BallAttributes = {
+  /**
+   * the constant speed of this object
+   * @default 1.25
+   */
+  constantSpeed?: number
   radius?: number
 } & GameObjectParameter
   & UserDefinedPhysics
@@ -23,6 +28,7 @@ function withDefault(attr: BallAttributes): BallAttributes {
     ...{
       hitBoxShape: 'circle',
       radius: 5,
+      constantSpeed: 1.25,
       x: defaultLayout.container.width / 2,
       y: defaultLayout.container.height / 2,
       ...ballBody,
@@ -33,20 +39,26 @@ function withDefault(attr: BallAttributes): BallAttributes {
 export class Ball extends Interactable(Materialized(GameObject)) implements Shapeable, Convertable {
   private trail: Trail
   private isGameStarted: Boolean
+  private physicsBody: Body
+  private constantSpeed: number
   private onCollisionCallback: (other: Body) => void
 
   constructor({ app, onCollisionCallback, ...attributes }: ctor) {
-    super(app, withDefault(attributes))
+    const attr = withDefault(attributes)
+    super(app, attr)
 
+    this.constantSpeed = attr.constantSpeed
     this.onCollisionCallback = onCollisionCallback
   }
 
   onLoad(physicsBody: Body) {
+    this.physicsBody = physicsBody
+
     if (this.key && typeof this.key === 'function') {
       this.key(' ').onRelease = () => {
         if (physicsBody && !this.isGameStarted) {
           Body.setVelocity(physicsBody, Vector.rotate(
-            { x: 1.5, y: 1.5 },
+            Vector.create(1, 1),
             Math.random(),
           ))
 
@@ -72,7 +84,26 @@ export class Ball extends Interactable(Materialized(GameObject)) implements Shap
     this.trail = new Trail(sprite)
   }
 
-  update(_delta: number): void {
+  fixedUpdate(_delta: number): void {
     this.trail.onTick(_delta)
+
+    const speed = this.physicsBody.speed
+    // keep the speed constant (matterjs does not have settings for that)
+    if (speed < this.constantSpeed) {
+      Body.setVelocity(
+        this.physicsBody,
+        Vector.mult(Vector.normalise(this.physicsBody.velocity), this.constantSpeed)
+      )
+    }
+
+    // some mumbo-jumbo here to spice up the gameplay!
+    const { x, y } = this.physicsBody.velocity
+    const m = this.constantSpeed - 0.5
+    if (x > m) { // (does not allow the angle to X-axis fall low)
+      Body.setVelocity(
+        this.physicsBody,
+        Vector.create((m - 0.2) * Math.random() + 0.2, y)
+      )
+    }
   }
 }
