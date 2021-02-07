22412a4c7180828dd6e26c05a6ed5994f12b8fac
dungtang93 <dungtang93@gmail.com>
2021-02-08 01:32:10 +0700

    add debug info

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,14 @@ import * as sapper from '@sapper/server'
 const { PORT, NODE_ENV, BASE_PATH } = process.env
 const dev = NODE_ENV === 'development'
 
+if (BASE_PATH) {
+	console.log(`base path: ${BASE_PATH}`)
+	console.log(`isDev? ${dev}`)
+} else {
+	console.log(`base path not being used!`)
+	console.log(`isDev? ${dev}`)
+}
+
 polka()
 	.use(
 		BASE_PATH ? '/pingpong' : '/', // BASE_PATH env variable is set during `npm run export`
